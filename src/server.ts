0cc051b3ff84982c9b41861e11c7d356d922bd64
OluC94 <coker.olu94@gmail.com>
2024-07-11 16:09:35 +0100

    cleanup, format, linting

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,15 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import {
-    addDummyDbItems,
-    addDbItem,
-    getAllDbItems,
-    getDbItemById,
-    DbItem,
-    updateDbItemById,
-} from "./db";
+import { addDummyDbItems } from "./db";
 import filePath from "./filePath";
-import phonebookData from "./data/phonebook.json"
-import { addPerson, getPersonById, isNotUniqueName, PhonebookItem, PhonebookItemWithId } from "./utils/utils";
+import phonebookData from "./data/phonebook.json";
+import {
+    addPerson,
+    getPersonById,
+    isNotUniqueName,
+    PhonebookItem,
+} from "./utils/utils";
 import morgan from "morgan";
 
 // loading in some dummy items into the database
@@ -24,17 +22,20 @@ const app = express();
 app.use(express.json());
 /** To allow 'Cross-Origin Resource Sharing': https://en.wikipedia.org/wiki/Cross-origin_resource_sharing */
 app.use(cors());
-app.use(morgan(function (tokens, req, res) {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms',
-      JSON.stringify(req.body)
-      
-    ].join(' ')
-  }))
+app.use(
+    morgan(function (tokens, req, res) {
+        return [
+            tokens.method(req, res),
+            tokens.url(req, res),
+            tokens.status(req, res),
+            tokens.res(req, res, "content-length"),
+            "-",
+            tokens["response-time"](req, res),
+            "ms",
+            JSON.stringify(req.body),
+        ].join(" ");
+    })
+);
 
 // read in contents of any environment variables in the .env file
 // Must be done BEFORE trying to access process.env...
@@ -45,7 +46,7 @@ const PORT_NUMBER = process.env.PORT ?? 4000;
 
 // API info page
 app.get("/", (req, res) => {
-    console.log()
+    console.log();
     const pathToFile = filePath("../public/index.html");
     res.sendFile(pathToFile);
 });
@@ -56,82 +57,39 @@ app.get("/api/persons", (req, res) => {
 });
 
 app.get("/info", (req, res) => {
-    const numberOfPeople = phonebookData.length
-    const date = new Date()
-    res.send(`<p>Phonebook has info for ${numberOfPeople} people</p><br/><p>${date}</p>`)
-})
-
-app.get<{id: string}>("/api/persons/:id", (req, res) => {
-    const personData = getPersonById(req.params.id)
-    const statusCode = personData === "not found" ? 404 : 200
-    res.status(statusCode).json(personData)
-})
-
-app.delete<{id: string}>("/api/persons/:id", (req, res) => {
-    const personData = getPersonById(req.params.id)
-    const statusCode = personData === "not found" ? 404 : 204
-    res.status(statusCode).json(personData)
-})
-
-app.post<{}, {}, PhonebookItem>("/api/persons/", (req ,res) => {
-   
-    const postData = req.body
-    if (isNotUniqueName(postData)){
-        res.status(400).json({eroor: "name must be unique"})
-        return
-    }
-
-    if (postData.name && postData.number){
-        const newEntry = addPerson(postData)
-        res.status(201).json(newEntry)
-        return
-    }
-
-
-    res.status(400).json({message: "error: incomplete data"})
-    
-})
-
-// POST /items
-app.post<{}, {}, DbItem>("/items", (req, res) => {
-    // to be rigorous, ought to handle non-conforming request bodies
-    // ... but omitting this as a simplification
-    const postData = req.body;
-    const createdSignature = addDbItem(postData);
-    res.status(201).json(createdSignature);
-});
-
-// GET /items/:id
-app.get<{ id: string }>("/items/:id", (req, res) => {
-    const matchingSignature = getDbItemById(parseInt(req.params.id));
-    if (matchingSignature === "not found") {
-        res.status(404).json(matchingSignature);
-    } else {
-        res.status(200).json(matchingSignature);
-    }
-});
-
-// DELETE /items/:id
-app.delete<{ id: string }>("/items/:id", (req, res) => {
-    const matchingSignature = getDbItemById(parseInt(req.params.id));
-    if (matchingSignature === "not found") {
-        res.status(404).json(matchingSignature);
-    } else {
-        res.status(200).json(matchingSignature);
-    }
-});
-
-// PATCH /items/:id
-app.patch<{ id: string }, {}, Partial<DbItem>>("/items/:id", (req, res) => {
-    const matchingSignature = updateDbItemById(
-        parseInt(req.params.id),
-        req.body
+    const numberOfPeople = phonebookData.length;
+    const date = new Date();
+    res.send(
+        `<p>Phonebook has info for ${numberOfPeople} people</p><br/><p>${date}</p>`
     );
-    if (matchingSignature === "not found") {
-        res.status(404).json(matchingSignature);
-    } else {
-        res.status(200).json(matchingSignature);
+});
+
+app.get<{ id: string }>("/api/persons/:id", (req, res) => {
+    const personData = getPersonById(req.params.id);
+    const statusCode = personData === "not found" ? 404 : 200;
+    res.status(statusCode).json(personData);
+});
+
+app.delete<{ id: string }>("/api/persons/:id", (req, res) => {
+    const personData = getPersonById(req.params.id);
+    const statusCode = personData === "not found" ? 404 : 204;
+    res.status(statusCode).json(personData);
+});
+
+app.post<{}, {}, PhonebookItem>("/api/persons/", (req, res) => {
+    const postData = req.body;
+    if (isNotUniqueName(postData)) {
+        res.status(400).json({ eroor: "name must be unique" });
+        return;
     }
+
+    if (postData.name && postData.number) {
+        const newEntry = addPerson(postData);
+        res.status(201).json(newEntry);
+        return;
+    }
+
+    res.status(400).json({ message: "error: incomplete data" });
 });
 
 app.listen(PORT_NUMBER, () => {
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,4 @@
-import phonebookData from "../data/phonebook.json"
+import phonebookData from "../data/phonebook.json";
 
 interface PhonebookItem {
     name: string;
@@ -6,27 +6,31 @@ interface PhonebookItem {
 }
 
 interface PhonebookItemWithId extends PhonebookItem {
-    id: string,
+    id: string;
 }
 
-const getPersonById = (id: string):PhonebookItemWithId | "not found" => {
-    const entry = phonebookData.find(person => person.id === id);
-    return entry ? entry : "not found"
-}
+const getPersonById = (id: string): PhonebookItemWithId | "not found" => {
+    const entry = phonebookData.find((person) => person.id === id);
+    return entry ? entry : "not found";
+};
 
-const addPerson = (data: PhonebookItem) : PhonebookItemWithId => {
-    const newId: number = Math.floor(Math.random()* (100 - phonebookData.length + 1) + phonebookData.length)
+const addPerson = (data: PhonebookItem): PhonebookItemWithId => {
+    const newId: number = Math.floor(
+        Math.random() * (100 - phonebookData.length + 1) + phonebookData.length
+    );
     const newPerson: PhonebookItemWithId = {
         id: newId.toString(),
-        ...data
-    }
-    phonebookData.push(newPerson)
-    return newPerson
-}
+        ...data,
+    };
+    phonebookData.push(newPerson);
+    return newPerson;
+};
 
-const isNotUniqueName = (data: PhonebookItem) : boolean => {
-    const matchingPeople: PhonebookItemWithId[] = phonebookData.filter(person => person.name === data.name)
+const isNotUniqueName = (data: PhonebookItem): boolean => {
+    const matchingPeople: PhonebookItemWithId[] = phonebookData.filter(
+        (person) => person.name === data.name
+    );
     return matchingPeople.length > 0;
-}
+};
 
-export {PhonebookItemWithId, PhonebookItem, getPersonById, addPerson, isNotUniqueName}
+export { PhonebookItem, getPersonById, addPerson, isNotUniqueName };
